6567f3af6f16880fbc3957602713bf4eadcbdd6c
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2026-09-19 17:41:04 +0100

    width range calculated and implemented

--- src/components/market-depth-table/MarketDepthTable.tsx
+++ src/components/market-depth-table/MarketDepthTable.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import '../placeholder/Placeholder.css';
 import './MarketDepthTable.css';
 import { MarketDepthRow } from '../market-depth/useMarketDepthData';
@@ -7,7 +7,19 @@ interface MarketDepthTableProp {
   data: MarketDepthRow[];
 }
 export const MarketDepthTable = ({data}: MarketDepthTableProp) => {
-  
+  // const[bidPrice, setBidPrice] = useState<number>(0);
+  // const previousBidPrice = useRef<number>(0);
+
+
+  const getMaximumLength = Math.max( ...data.map(row => row.bidQuantity), ...data.map(row => row.offerQuantity));
+    
+
+  const getQuantityWidth = (quantity: number) => {
+    if(quantity === 0) {
+      return 0;
+    }
+    return (quantity / getMaximumLength ) * 100;
+  }
   return (
     <div className="Placeholder Table" >
       
@@ -17,26 +29,52 @@ export const MarketDepthTable = ({data}: MarketDepthTableProp) => {
             <tr id='Table-mainHeader'>
               <th > </th>
               <th colSpan={2} style={{border: "1px solid red"}} >Bid</th>
-              <th colSpan={2}>Offer</th>
+              <th colSpan={2}>Ask</th>
             </tr>
             <tr id='Table-subHeader'>
               <th></th>
               <th>Quantity</th>
               <th>Price</th>
-              <th>Quantity</th>
               <th>Price</th>
+              <th>Quantity</th>
             </tr>
           </thead>
 
           <tbody>
             {data.map((row, index) => (
               
-              <tr key={index}>
-                <td>{index}</td>
-                <td>{row.bidQuantity}</td>
-                <td>{row.bid}</td>
-                <td>{row.offerQuantity}</td>
+              <tr key={index} >
+                <td>
+                  <div> {index} </div>
+                </td>
+                {/* bid quantity */}
+                <td className='Table-quantity'>
+                  <div style={{width: `${getQuantityWidth(row.bidQuantity)}%`}}>
+                    {row.bidQuantity}
+                  </div>
+                </td>
+
+                {/* bid price */}
+                <td className='Price'>
+                  <div>
+                    <div>
+                    up
+                  </div>
+                  <div >
+                    {row.bid}
+                  </div>
+                  </div>
+                </td>
+
+                {/* offer price */}
                 <td>{row.offer}</td>
+
+                {/* offer quantity */}
+                <td className='Table-quantity'>
+                  <div style={{width: `${getQuantityWidth(row.offerQuantity)}%`, backgroundColor:"red"}}>
+                    {row.offerQuantity}
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
